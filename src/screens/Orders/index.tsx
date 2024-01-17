a6778eb7dff3d8b301277b5082f8f8c90f491994
CaioChristhian <caiochristhianz@gmail.com>
2024-01-17 19:17:03 -0300

    feat: adding orders components

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
 import * as S from './styles';
+import { Text } from '../../components/Text';
 
 export function Orders(){
 	return (
 		<S.Container>
+			<S.Header>
+				<Text size={24} weight='600'>Pedidos</Text>
+			</S.Header>
 
+			<S.OnGoingOrdersContainer>
+				<Text weight='600' color='#666666' size={18} style={{ marginBottom: 24 }}>
+					Em andamento
+				</Text>
+
+				<S.OnGoingOrdersContent>
+					<S.HeaderContent>
+						<Text weight='600'>Mesa123</Text>
+
+						<S.StatusCard>
+							<Text>Pronto!</Text>
+						</S.StatusCard>
+					</S.HeaderContent>
+
+					<S.IngredientsContainer>
+						<S.IngredientsContent>
+							<Text color='#999999' style={{ paddingRight: 8 }}>1x</Text>
+							<Text>Frango com Catupiry</Text>
+						</S.IngredientsContent>
+
+						<S.IngredientsContent>
+							<Text color='#999999' style={{ paddingRight: 8 }}>2x</Text>
+							<Text>Quatro Queijos</Text>
+						</S.IngredientsContent>
+					</S.IngredientsContainer>
+				</S.OnGoingOrdersContent>
+
+			</S.OnGoingOrdersContainer>
+
+			<S.BeforeOrdersContainer>
+
+			</S.BeforeOrdersContainer>
 		</S.Container>
 	);
 }
--- src/screens/Orders/styles.ts
+++ src/screens/Orders/styles.ts
@@ -1,5 +1,50 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.SafeAreaView`
 	flex: 1;
+	padding: 24px;
+	background-color: #FAFAFA;
 `;
+
+export const Header = styled.View`
+	margin-top: 20px;
+	margin-bottom: 42px;
+`;
+
+export const OnGoingOrdersContainer = styled.View`
+
+`;
+
+export const OnGoingOrdersContent = styled.View`
+	background-color: #FFFFFF;
+	width: 100%;
+	height: 144px;
+	padding: 24px;
+	border-radius: 8px;
+`;
+
+export const HeaderContent = styled.View`
+	flex-direction: row;
+	justify-content: space-between;
+	margin-bottom: 24px;
+`;
+
+export const StatusCard = styled.View`
+	background-color: #30D787;
+	padding: 6px;
+	border-radius: 4px;
+`;
+
+export const IngredientsContainer = styled.View`
+
+	gap: 8px;
+`;
+
+export const IngredientsContent = styled.View`
+	flex-direction: row;
+`;
+
+export const OrdersContainer = styled.View``;
+
+
+export const BeforeOrdersContainer = styled.View``;
