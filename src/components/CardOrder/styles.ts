6c26a67bfb0a6bf1e40e7d81fa87de589f387950
CaioChristhian <caiochristhianz@gmail.com>
2024-01-30 11:35:32 -0300

    feat: integrando completamente orders com a api, e adicionando StatusModal

--- src/components/CardOrder/styles.ts
+++ src/components/CardOrder/styles.ts
@@ -16,13 +16,14 @@ export const HeaderContent = styled.View`
 
 export const StatusWrapper = styled.TouchableOpacity`
 	background-color: rgba(48,215,135, 0.05);
+	border-radius: 4px;
 `;
 
 export const StatusCard = styled.View`
 	padding: 6px;
-	border-radius: 4px;
 	flex-direction: row;
 	align-items: center;
+	justify-content: center;
 `;
 
 export const IngredientsContainer = styled.View`
@@ -33,3 +34,47 @@ export const IngredientsContent = styled.View`
 	flex-direction: row;
 `;
 
+export const ModalBody = styled.View`
+	gap: 12px;
+	background-color: #FFFFFF;
+	border-radius: 8px;
+`;
+
+export const ModalOptions = styled.View`
+	align-items: center;
+	justify-content: center;
+	gap: 12px;
+	padding: 24px;
+`;
+
+export const Overlay = styled.KeyboardAvoidingView`
+	background: rgba(0, 0, 0, 0.7);
+	flex: 1;
+	align-items: stretch;
+	justify-content: center;
+	padding: 0 24px;
+`;
+
+export const HeaderModal = styled.View`
+	width: 100%;
+	align-items: flex-end;
+	background-color: #D73035;
+	border-top-right-radius: 8px;
+	border-top-left-radius: 8px;
+`;
+
+export const StatusOptionButton = styled.TouchableOpacity`
+	align-items: center;
+	flex-direction: row;
+	justify-content: center;
+	padding: 12px;
+	border-radius: 4px;
+	width: 100%;
+`;
+
+export const CloseButton = styled.TouchableOpacity`
+	align-items: center;
+	justify-content: center;
+	margin: 24px;
+
+`;
